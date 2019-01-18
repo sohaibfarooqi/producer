7f7f5955ffe33c862be09607863a0b83ce5ef931
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-01-18 17:40:31 +0800

    setting celery_always_eager=True default option

--- file_reader/tasks/config.py
+++ file_reader/tasks/config.py
@@ -9,6 +9,6 @@ class CeleryConfig:
     enable_utc = True
     timezone = "UTC"
 
-    task_always_eager = os.environ.get('CELERY_ALWAYS_EAGER', False)
-    task_eager_propagates = os.environ.get('CELERY_EAGER_PROPAGATES', False)
+    task_always_eager = os.environ.get('CELERY_ALWAYS_EAGER', True)
+    task_eager_propagates = os.environ.get('CELERY_EAGER_PROPAGATES', True)
     task_create_missing_queues = True
