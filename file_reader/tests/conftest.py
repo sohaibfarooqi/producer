b5a1de7006ee9e2915e83e3d9e18338ac9f3d843
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-01-18 23:26:14 +0800

    added test fixtures

--- file_reader/tests/conftest.py
+++ file_reader/tests/conftest.py
@@ -9,16 +9,55 @@ def app():
     """
     yield create_app(testing=True)
 
-@pytest.fixture(scope='session')
-def empty_file():
-    """
-    This fixture creates Flask app with testing
-    configurations.
-    """
-    file_name = 'test.csv'
+@pytest.fixture
+def invalid_file():
+    file_name = 'invalid_test.txt'
     file = open(file_name, 'w')
     file.close()
     yield open(file_name, 'rb')
+    os.remove(file_name)
+
+@pytest.fixture
+def valid_file():
+    file_name = 'valid_test.csv'
+    file = open(file_name, 'w')
+    writer = csv.writer(file)
+    writer.writerow(['abc', 'abc@def.com'])
+    file.close()
+    yield open(file_name, 'rb')
+    os.remove(file_name)
+
+@pytest.fixture
+def file_with_header():
+    file_name = 'file_with_header.csv'
+    file = open(file_name, 'w')
+    writer = csv.writer(file)
+    writer.writerow(['name', 'email'])
+    writer.writerow(['abc', 'abc@def.com'])
+    file.close()
+    yield open(file_name, 'r')
+    os.remove(file_name)
+
+@pytest.fixture
+def file_with_seperator():
+    file_name = 'file_with_seperator.csv'
+    file = open(file_name, 'w')
+    writer = csv.writer(file, delimiter='|')
+    writer.writerow(['abc', 'abc@def.com'])
+    file.close()
+    yield file_name
+    os.remove(file_name)
+
+@pytest.fixture
+def custom_file_format():
+    file_name = 'custom_file_format.csv'
+    file = open(file_name, 'w')
+    writer = csv.writer(file, delimiter='|')
+    writer.writerow(['email', 'name'])
+    writer.writerow(['abc@def.com', 'abc'])
+    writer.writerow(['abc@def.com', 'abc'])
+    file.close()
+    yield file_name
     os.remove(file_name)
 
 
