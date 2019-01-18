ea8535b0330bb379c1300e54d88a4c64ec1771fc
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-01-18 23:27:01 +0800

    api view function test cases

--- file_reader/tests/test_routes.py
+++ file_reader/tests/test_routes.py
@@ -5,9 +5,16 @@ def test_without_file(app):
     rv = app.test_client().post(url_for('upload'))
     rv.status_code == 400
 
-def test_empty_file(app, empty_file):
+def test_invalid_file_format(app, invalid_file):
   data = {}
-  data['file'] = (empty_file, empty_file.name)
+  data['file'] = (invalid_file, invalid_file.name)
   with app.app_context() as ctx:
     rv = app.test_client().post(url_for('upload'), data=data)
-    assert rv.status_code == 400
+    rv.status_code == 406
+
+def test_successful_request(app, valid_file):
+  data = {}
+  data['file'] = (valid_file, valid_file.name)
+  with app.app_context() as ctx:
+    rv = app.test_client().post(url_for('upload'), data=data)
+    assert rv.status_code == 200
