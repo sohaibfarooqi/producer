d53e30bda706e3170b089414787f9b6d1992ecb0
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-01-20 14:41:25 +0800

    renamed package to producer

---
+++ producer/__init__.py
@@ -0,0 +1,6 @@
+from .create_app import create_app
+from .producer import celery_app, Producer
+
+if __name__ == "__main__":
+  app = create_app()
+  app.run()
---
+++ producer/config.py
@@ -0,0 +1,38 @@
+import os
+
+class Config(object):
+    """
+    Base config object.
+    """
+    DEBUG = False
+    TESTING = False
+    DATA_FILE_PATH = "data_files"
+
+class DevelopmentConfig(Config):
+    """
+    Development config class. Enable hot
+    reloading to expedite development.
+    """
+    DEBUG = True
+
+class TestingConfig(Config):
+    """
+    Testing config class to facilitate unit test
+    with Flask test client.
+    """
+    TESTING = True
+    SERVER_NAME = "localhost"
+
+class CeleryConfig:
+    """
+    Configuration for Celery
+    """
+    broker_url = os.environ.get('CELERY_BROKER_URL', 'pyamqp://')
+
+    enable_utc = True
+    timezone = "UTC"
+
+    task_always_eager = os.environ.get('CELERY_ALWAYS_EAGER', False)
+    task_eager_propagates = os.environ.get('CELERY_EAGER_PROPAGATES', False)
+    task_create_missing_queues = True
+
